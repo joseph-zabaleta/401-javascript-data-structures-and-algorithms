d3057f27c862c30dee12fba83eebd05cecb9db2a
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-07-31 11:17:01 -0700

    3.2 Complete

--- code-challenges/301/03-TwoDimensionalArrays/challenges-03.test.js
+++ code-challenges/301/03-TwoDimensionalArrays/challenges-03.test.js
@@ -41,8 +41,20 @@ const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
-  // Solution code here...
+    let output = [];
 
+    for (let i in hoursOpen) {
+
+        let currentValue = 0;
+
+        stores.forEach(element => {
+            currentValue += element[i];
+        });
+
+        output.push(currentValue);
+    }
+
+    return output;
 };
 
 /* ------------------------------------------------------------------------------------------------
